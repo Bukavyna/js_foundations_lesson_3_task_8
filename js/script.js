e5a1ad150a06758afb39badd66b8039aa91355bd
Taras <luchakt2@gmail.com>
2023-09-03 22:03:35 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 "use strict"
 
+let a = +prompt(`Введіть число`);
+let b = +prompt(`Введіть степінь`);
+let result = 1;
+for (let i=1; i<=b; i++) {
+	result = result * a;
+}
+document.write(`<h1>${result}</h1>`);
