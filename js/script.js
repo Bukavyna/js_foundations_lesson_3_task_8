a811cad3d898cfbb15cae330428713a089ca2dfd
Taras <luchakt2@gmail.com>
2023-09-12 22:48:56 +0300

    add degree

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 "use strict"
 
+//Піднесення до степеня
+/*
+
 let a = +prompt(`Введіть число`);
 let b = +prompt(`Введіть степінь`);
 let result = 1;
 for (let i=1; i<=b; i++) {
 	result = result * a;
 }
+document.write(`<h1>${result}</h1>`);
+
+ */
+
+//Піднесення до від'ємного степеня
+
+let a = +prompt(`Введіть число`);
+let b = +prompt(`Введіть степінь`);
+function power(a, b) {
+	let result = 1;
+	for(let i = 0; i > b; i--) {
+		result *= a;
+	}
+	return 1 / result;
+}
+let result = power(a, b);
 document.write(`<h1>${result}</h1>`);
